86c26ae12ac83438c46c0302fa6b5f8adcd29f57
Chamoda De Silva <chamoda12345678@gmail.com>
2022-03-27 10:20:34 +0530

    validated fields

--- app/src/main/java/com/example/android_front/MainActivity.java
+++ app/src/main/java/com/example/android_front/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String url="https://concal-test.herokuapp.com/";//1650 0 143.6  163.8 0 1005 900.9 3
-        Button btn=findViewById(R.id.btnPredict);
+        String url="https://concal-backend-dp.herokuapp.com/";//1650 0 143.6  163.8 0 1005 900.9 3
+        Button btn=findViewById(R.id.btnPredict);//https://concal-backend-dp.herokuapp.com/
 
         TextView output=findViewById(R.id.txtAnswer);
         EditText input1=findViewById(R.id.editTextInput);
@@ -46,38 +46,66 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject=new JSONObject((response));
-                            String data= jsonObject.getString("output");
-                            output.setText(data.toString());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                if (input1.getText().toString().equals("0") && input2.getText().toString().equals("0") && input3.getText().toString().equals("0") && input4.getText().toString().equals("0") && input5.getText().toString().equals("0") && input6.getText().toString().equals("0") && input7.getText().toString().equals("0") && input8.getText().toString().equals("0")) {
+                    Toast toast=Toast.makeText(getApplicationContext(),"Empty Field",Toast.LENGTH_LONG);
+                    toast.show();
+
+                }else if (notANumInRange(input1.getText().toString()) || notANumInRange(input2.getText().toString()) || notANumInRange(input3.getText().toString()) || notANumInRange(input4.getText().toString()) || notANumInRange(input5.getText().toString()) || notANumInRange(input6.getText().toString()) || notANumInRange(input7.getText().toString()) || notANumInRange(input8.getText().toString())) {
+                    Toast toast=Toast.makeText(getApplicationContext(),"All should be a number",Toast.LENGTH_LONG);
+                    toast.show();
+
+                }else if(input8.getText().toString().equals("0") ){
+                    Toast toast=Toast.makeText(getApplicationContext(),"Age can't be zero",Toast.LENGTH_LONG);
+                    toast.show();
+                } else{
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject((response));
+                                String data = jsonObject.getString("output");
+                                output.setText(data.toString() + " MPa");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_LONG);
                         }
 
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this,error.getMessage().toString(),Toast.LENGTH_LONG);
-                    }
-
-                }){
-                    @Override
-                    protected Map<String,String> getParams(){
-                        Map<String,String> params=new HashMap<String,String>();
-                        String input=input1.getText().toString()+" "+input2.getText().toString()+" "+input3.getText().toString()+" "+input4.getText().toString()+" "+input5.getText().toString()+" "+input6.getText().toString()+" "+input7.getText().toString()+" "+input8.getText().toString();
-                        params.put("inputs",input);
-                        return params;
-                    }
-                };
-                RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            String input = input1.getText().toString() + " " + input2.getText().toString() + " " + input3.getText().toString() + " " + input4.getText().toString() + " " + input5.getText().toString() + " " + input6.getText().toString() + " " + input7.getText().toString() + " " + input8.getText().toString();
+                            params.put("inputs", input);
+                            return params;
+                        }
+                    };
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                 queue.add(stringRequest);
+            }
 
             }
         });
 
+    }
+
+    private boolean notANumInRange(String strNum){
+        if (strNum == null) {
+            return true;//validate num is not null
+        }
+        try {
+            double d = Double.parseDouble(strNum);//validate string is a num
+            if(d<0){
+                return true;//validate num cant be minus
+            }
+        } catch (NumberFormatException nfe) {
+            return true;
+        }
+        return false;
     }
 }
